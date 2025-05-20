bb683ac9fe59b17fd86be249180c8475562b9862
ASUS <mustafasharaf100@gmail.com>
2025-05-20 08:10:24 +0300

    Add project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SpotDifferenceGames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxLeft = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRight = new System.Windows.Forms.PictureBox();
+            this.ChoosePicture = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.labelAttempts = new System.Windows.Forms.Label();
+            this.comboBoxLevel = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLeft
+            // 
+            this.pictureBoxLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxLeft.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxLeft.Name = "pictureBoxLeft";
+            this.pictureBoxLeft.Size = new System.Drawing.Size(550, 550);
+            this.pictureBoxLeft.TabIndex = 0;
+            this.pictureBoxLeft.TabStop = false;
+            this.pictureBoxLeft.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox_Click);
+            // 
+            // pictureBoxRight
+            // 
+            this.pictureBoxRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxRight.Location = new System.Drawing.Point(568, 12);
+            this.pictureBoxRight.Name = "pictureBoxRight";
+            this.pictureBoxRight.Size = new System.Drawing.Size(550, 550);
+            this.pictureBoxRight.TabIndex = 1;
+            this.pictureBoxRight.TabStop = false;
+            this.pictureBoxRight.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox_Click);
+            // 
+            // ChoosePicture
+            // 
+            this.ChoosePicture.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ChoosePicture.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChoosePicture.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.ChoosePicture.Location = new System.Drawing.Point(12, 580);
+            this.ChoosePicture.Name = "ChoosePicture";
+            this.ChoosePicture.Size = new System.Drawing.Size(150, 32);
+            this.ChoosePicture.TabIndex = 2;
+            this.ChoosePicture.Text = "Choose a picture";
+            this.ChoosePicture.UseVisualStyleBackColor = false;
+            this.ChoosePicture.Click += new System.EventHandler(this.ChoosePicture_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 12F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(407, 580);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of detected differences:        ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 12F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(407, 613);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number of remaining differences:      ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.labelTimer.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimer.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.labelTimer.Location = new System.Drawing.Point(676, 613);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(42, 24);
+            this.labelTimer.TabIndex = 6;
+            this.labelTimer.Text = "Timer";
+            this.labelTimer.Click += new System.EventHandler(this.labelTimer_Click);
+            // 
+            // labelAttempts
+            // 
+            this.labelAttempts.AutoSize = true;
+            this.labelAttempts.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.labelAttempts.Font = new System.Drawing.Font("Agency FB", 12F);
+            this.labelAttempts.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.labelAttempts.Location = new System.Drawing.Point(676, 581);
+            this.labelAttempts.Name = "labelAttempts";
+            this.labelAttempts.Size = new System.Drawing.Size(128, 24);
+            this.labelAttempts.TabIndex = 7;
+            this.labelAttempts.Text = "Remaining attempts:";
+            // 
+            // comboBoxLevel
+            // 
+            this.comboBoxLevel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.comboBoxLevel.Font = new System.Drawing.Font("Agency FB", 12F);
+            this.comboBoxLevel.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.comboBoxLevel.FormattingEnabled = true;
+            this.comboBoxLevel.Location = new System.Drawing.Point(178, 581);
+            this.comboBoxLevel.Name = "comboBoxLevel";
+            this.comboBoxLevel.Size = new System.Drawing.Size(200, 32);
+            this.comboBoxLevel.TabIndex = 8;
+            this.comboBoxLevel.Text = "Choose a level";
+            this.comboBoxLevel.SelectedIndexChanged += new System.EventHandler(this.comboBoxLevel_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 704);
+            this.Controls.Add(this.comboBoxLevel);
+            this.Controls.Add(this.labelAttempts);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ChoosePicture);
+            this.Controls.Add(this.pictureBoxRight);
+            this.Controls.Add(this.pictureBoxLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLeft;
+        private System.Windows.Forms.PictureBox pictureBoxRight;
+        private System.Windows.Forms.Button ChoosePicture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Label labelAttempts;
+        private System.Windows.Forms.ComboBox comboBoxLevel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using NAudio.Wave; 
+
+namespace SpotDifferenceGames
+{
+    public partial class Form1 : Form
+    {
+      private int imageLoadCount = 0;
+      private Timer gameTimer;
+      private Timer displayTimer;   
+      private int timeLeft;
+      private int wrongAttempts = 0;
+      private const int MaxWrongAttempts = 6;
+      private int gameLevel = 0; 
+      private List<(Point location, bool isDifference, bool isLeft)> clickPoints = new List<(Point, bool, bool)>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load; 
+   
+            pictureBoxLeft.MouseClick += PictureBox_Click;
+            pictureBoxRight.MouseClick += PictureBox_Click;
+
+            pictureBoxLeft.Paint += PictureBox_Paint;
+            pictureBoxRight.Paint += PictureBox_Paint;
+        }
+
+        private void ChoosePicture_Click(object sender, EventArgs e)
+        {
+            if (gameLevel == 0)
+            {
+                MessageBox.Show("الرجاء اختيار مستوى قبل تحميل الصور.", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Image selectedImage = Image.FromFile(openFileDialog.FileName);
+
+                    if (imageLoadCount == 0)
+                    {
+                        pictureBoxLeft.Image = selectedImage;
+                        pictureBoxLeft.Width = selectedImage.Width;
+                        pictureBoxLeft.Height = selectedImage.Height;
+                        pictureBoxLeft.SizeMode = PictureBoxSizeMode.Normal;
+                        imageLoadCount++;
+                    }
+                    else if (imageLoadCount == 1)
+                    {
+                        pictureBoxRight.Image = selectedImage;
+                        pictureBoxRight.Width = selectedImage.Width;
+                        pictureBoxRight.Height = selectedImage.Height;
+                        pictureBoxRight.SizeMode = PictureBoxSizeMode.Normal;
+                        imageLoadCount++;
+
+                        if (gameLevel >= 2)
+                            StartGameTimer();
+
+                        ChoosePicture.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void PictureBox_Click(object sender, MouseEventArgs e)
+        {
+            var clickedBox = sender as PictureBox;
+            bool isLeft = clickedBox == pictureBoxLeft;
+
+            if (pictureBoxLeft.Image == null || pictureBoxRight.Image == null)
+                return;
+
+            int imageX = e.X;
+            int imageY = e.Y;
+
+            Bitmap bmpLeft = (Bitmap)pictureBoxLeft.Image;
+            Bitmap bmpRight = (Bitmap)pictureBoxRight.Image;
+
+            bool isDifferent = IsRegionDifferent(bmpLeft, bmpRight, imageX, imageY);
+
+            clickPoints.Add((e.Location, isDifferent, isLeft));
+
+            Point mirroredPoint = new Point(e.X, e.Y);
+            clickPoints.Add((mirroredPoint, isDifferent, !isLeft));
+
+            pictureBoxLeft.Invalidate();
+            pictureBoxRight.Invalidate();
+
+            if (isDifferent)
+            {
+                PlayMp3("E:\\Fourth year\\Semester2(2024-2025)\\نظم الوسائط المتعددة\\Multimedia Systems\\true answers .mp3");
+                label1.Text = $"Number of detected differences: {GetDiscoveredDifferencesCount()}";
+                label2.Text = $"Number of detected differences: {5 - GetDiscoveredDifferencesCount()}";
+                if (GetDiscoveredDifferencesCount()==5)
+                {
+                    MessageBox.Show($"مبروك لقد ربحت.", "مبروك", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    this.Close();
+                    return;
+                }
+
+            }
+            else
+            {
+                PlayMp3("E:\\Fourth year\\Semester2(2024-2025)\\نظم الوسائط المتعددة\\Multimedia Systems\\wrong answers .mp3");
+                if (gameLevel == 3|| gameLevel == 2) 
+                {
+                    wrongAttempts++;
+                    UpdateAttemptsLabel();
+                    if (wrongAttempts >= MaxWrongAttempts)
+                    {
+                        displayTimer?.Stop();
+                        gameTimer?.Stop();
+
+                        MessageBox.Show($"لقد خسرت اللعبة! تجاوزت الحد الأقصى للمحاولات (3).", "خسارة", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        this.Close();
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+            bool isLeft = pb == pictureBoxLeft;
+
+            foreach (var (location, isDifference, pointIsLeft) in clickPoints)
+            {
+                if (isDifference)
+                {
+                    e.Graphics.DrawEllipse(Pens.Green, location.X - 20, location.Y - 20, 40, 40);
+                }
+                else
+                {
+                    e.Graphics.DrawLine(Pens.Red, location.X - 15, location.Y - 15, location.X + 15, location.Y + 15);
+                    e.Graphics.DrawLine(Pens.Red, location.X - 15, location.Y + 15, location.X + 15, location.Y - 15);
+                }
+            }
+        }
+
+
+
+        private int ColorDifference(Color c1, Color c2)
+        {
+            return Math.Abs(c1.R - c2.R) + Math.Abs(c1.G - c2.G) + Math.Abs(c1.B - c2.B);
+        }
+
+        private bool IsRegionDifferent(Bitmap left, Bitmap right, int centerX, int centerY, int regionSize = 20, int threshold = 30)
+        {   int half = regionSize / 2;
+            int diffSum = 0;
+            int count = 0;
+
+            for (int y = -half; y <= half; y++)
+            {
+                for (int x = -half; x <= half; x++)
+                {
+                    int px = centerX + x;
+                    int py = centerY + y;
+
+                    if (px < 0 || py < 0 || px >= left.Width || py >= left.Height) continue;
+
+                    Color c1 = left.GetPixel(px, py);
+                    Color c2 = right.GetPixel(px, py);
+                    diffSum += ColorDifference(c1, c2);
+                    count++;
+                }
+            }
+            int averageDiff = diffSum / count;
+            return averageDiff > threshold;
+        }
+
+        private void PlayMp3(string path)
+        {
+            var reader = new AudioFileReader(path);
+            var player = new WaveOutEvent();
+
+            player.Init(reader);
+            player.Play();
+
+            // تخلص من الموارد عند انتهاء التشغيل
+            player.PlaybackStopped += (s, e) =>
+            {
+                player.Dispose();
+                reader.Dispose();
+            };
+        }
+
+        private void StartGameTimer()
+        {
+            if (gameLevel >= 2) 
+            {
+                timeLeft = 60;
+                gameTimer = new Timer();
+                gameTimer.Interval = 60000;
+                gameTimer.Tick += GameTimer_Tick;
+                gameTimer.Start();
+
+                displayTimer = new Timer();
+                displayTimer.Interval = 1000;
+                displayTimer.Tick += DisplayTimer_Tick;
+                displayTimer.Start();
+                UpdateTimerLabel();
+            }
+            if (gameLevel == 3)
+            {
+                wrongAttempts = 0;
+                UpdateAttemptsLabel();
+            }
+            else
+            {
+                labelAttempts.Text = ""; 
+            }
+        }
+
+        private void UpdateTimerLabel()
+        {
+            int minutes = timeLeft / 60;
+            int seconds = timeLeft % 60;
+            labelTimer.Text = $"Remaining Time : {minutes:D2}:{seconds:D2}";
+        }
+        private void UpdateAttemptsLabel()
+        {
+            labelAttempts.Text = $"Remaining Attempts: {(MaxWrongAttempts - wrongAttempts)/2}";
+        }
+
+        private void DisplayTimer_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+
+            if (timeLeft <= 0)
+            {
+                displayTimer.Stop();
+            }
+
+            UpdateTimerLabel();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            gameTimer.Stop();
+            MessageBox.Show("انتهى الوقت! لقد خسرت اللعبة.", "انتهى الوقت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close(); 
+        }
+        private int GetDiscoveredDifferencesCount()
+        {
+            var uniquePoints = new HashSet<Point>();
+
+            foreach (var (location, isDifference, isLeft) in clickPoints)
+            {
+                if (isDifference && isLeft)
+                {
+                    uniquePoints.Add(location);
+                }
+            }
+
+            return uniquePoints.Count;
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {//الفروقات المكتشفة   
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        { //الفروقات المتبقية
+        }
+
+        private void labelTimer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            gameLevel = comboBoxLevel.SelectedIndex + 1;
+            switch (gameLevel)
+            {
+                case 1:
+                    labelTimer.Visible = false;
+                    labelAttempts.Visible = false;
+                    break;
+
+                case 2:
+                    labelTimer.Visible = true;
+                    labelAttempts.Visible = false;
+                    break;
+
+                case 3:
+                    labelTimer.Visible = true;
+                    labelAttempts.Visible = true;
+                    break;
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxLevel.Items.Add("Level 1 without timer");
+            comboBoxLevel.Items.Add("Level 2 is timer only.");
+            comboBoxLevel.Items.Add("Level 3 timer and Points");
+          
+
+        }
+
+
+    }
+}
